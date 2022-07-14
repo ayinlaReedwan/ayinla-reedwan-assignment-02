4f7f0160dbbc5576358f44371d3cdce42498d24e
Ayinla Reedwan <ayinlareedwan@gmail.com>
2022-07-14 00:22:11 +0000

    attempt 1: -challenge 1: used the reduce method to loop over the array and store its value in an acccumulator which also serve as the first parameter in the next addition.
    -challange 2: used the for loop to iterare over the items of the array while checking if their modulus leaves no remainder.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,10 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  const sum = arrayOfNumbers.reduce ((a,b) => a+b);
+return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,7 +29,13 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+      let count = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    if(arrayOfNumbers[i] % 2 === 0){
+      count++
+    }
+  }
+    return count;
 }
 // assignment.countEvenNumbers = countEvenNumbers;
 
